5d1e8263ff182b211c51cc83b39e954774ce4d07
Bjørnar <bjornar.frigaard@gmail.com>
2025-12-09 18:20:42 +0100

    Refactor card styles and add header flip effect
    Split global and card-specific CSS into separate files, renamed style.css to front-page-card.css, and created css/style.css for global styles. Updated card.js to toggle a flip effect on card headers. Modified index.html to use the new CSS files and improved header text.

--- card.js
+++ card.js
@@ -1,7 +1,11 @@
 const cards = document.querySelectorAll(".card");
+const cardsHeader = document.querySelectorAll(".heading-icon");
 
-cards.forEach((card) => {
+cards.forEach((card, index) => {
   card.addEventListener("click", () => {
     card.classList.toggle("is-flipped");
+
+    const header = cardsHeader[index];
+    header.classList.toggle("heading-icon-is-flipped");
   });
 });
